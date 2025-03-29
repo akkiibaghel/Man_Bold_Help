b98ee14707843824bbea21057b68e8715a06e684
Bipin Kumar Yadav <69725182+bipinkumaryadav@users.noreply.github.com>
2025-03-29 13:23:37 +0530

    Allowed host updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    allowedHosts: ["manhelpwebsite.onrender.com"] // Allows access from any origin
+  }
 });
